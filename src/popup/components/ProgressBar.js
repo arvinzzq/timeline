7c2eaf5d58930e6e4a745fd18a14808643b2c3fe
zhongzhiqiang <zqzhong1211@163.com>
2018-05-08 23:38:24 +0800

    feat: add percent validation.

--- src/popup/components/ProgressBar.js
+++ src/popup/components/ProgressBar.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const calcProgress = (beginTime, endTime) => `${(100 * ((new Date()).getTime() - beginTime) /
-   (endTime - beginTime)).toFixed(2)}%`
+const calcProgress = (beginTime, endTime) => {
+  const date = (new Date()).getTime();
+  const percentProgress = (100 * (date - beginTime) /
+  (endTime - beginTime)).toFixed(2);
+  return `${percentProgress < 100 ? percentProgress : 100}%`
+}
 
 const getStyleProgressBarWrap = (height) => ({
   height,
