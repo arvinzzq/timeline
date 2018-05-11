54a2559cc1f7bb50a5503370c3a82ccf9d83844f
zhongzhiqiang <zqzhong1211@163.com>
2018-05-11 14:48:08 +0800

    refactor: modify package.json file.

--- src/popup/App.js
+++ src/popup/App.js
@@ -1,4 +1,4 @@
-import React, {PureComponent, Component} from 'react';
+import React, { PureComponent, Component } from 'react';
 import TaskBoard from './components/TaskBoard';
 
 const App = () => <TaskBoard />;
